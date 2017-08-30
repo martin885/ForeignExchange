e035187567af915ca190e5fe4030d69a90b9beee
MARTIN TORRES <mat885@hotmail.com>
2017-08-29 22:19:30 -0300

    initial project

---
+++ ForeignExchange2/ForeignExchange2/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ForeignExchange2.ViewModels;
+
+namespace ForeignExchange2.Infrastructure
+{
+    class InstanceLocator
+    {
+        public MainViewModel Main { get; set; }
+        public InstanceLocator()
+        {
+            Main = new MainViewModel();
+        }
+    }
+}
---
+++ ForeignExchange2/ForeignExchange2/ViewModels/MainViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForeignExchange2.ViewModels
+{
+   public class MainViewModel
+    {
+        public MainViewModel()
+        {
+
+        }
+    }
+}
